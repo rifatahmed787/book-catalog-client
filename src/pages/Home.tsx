cf024051ecfe1faaabd313f985bf7de26fb1639c
Rifat <takdircoders@gmail.com>
2023-08-27 17:17:14 +0600

    home banner

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,16 +1,15 @@
-import HeroBanner from "@/components/Home/HeroBanner";
+import BannerSwiper from "@/components/Home/BannerSwiper";
 import LatestBooks from "@/components/Home/LatestBooks";
 
 const Home = () => {
-	return (
-		<div>
-			<HeroBanner />
-			<div className="bg-white pt-[210px]  md:pt-[283px] pb-[100px] -mt-[190px]">
-				<LatestBooks />
-			</div>
-		</div>
-	);
+  return (
+    <div>
+      <BannerSwiper />
+      <div className="bg-white pt-[210px]  md:pt-[283px] pb-[100px] -mt-[190px]">
+        <LatestBooks />
+      </div>
+    </div>
+  );
 };
 
 export default Home;
-
